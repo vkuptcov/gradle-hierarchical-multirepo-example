96bc99992958a2b258cbf2ba51cca94b50a99273
Vladimir Kuptsov <vkuptcov@gmail.com>
2017-05-03 09:13:27 +0200

    + Main in module

---
+++ modules/number-utils/src/main/java/org/sample/numberutils/Main.java
@@ -0,0 +1,10 @@
+package org.sample.numberutils;
+
+import org.sample.common.Common;
+
+public class Main {
+
+    public static void main(String ... args) {
+        System.out.println(Common.CONSTANT);
+    }
+}
